ff2704c3cbab0041a07d992041b2ede36d613eac
Matthew Henry <henrym2@tcd.ie>
2020-01-05 14:13:38 +0000

    Finalize Node auth example

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const telnyx = require("telnyx")("YOUR_API_KEY")
-const moment = require('moment');
 const config = require('./config.json');
+const telnyx = require("telnyx")(config.API_KEY)
+const moment = require('moment');
 const express = require('express');
 const expressNunjucks = require('express-nunjucks');
+const crypto = require("crypto");
 
 const app = express();
 app.use(express.urlencoded());
@@ -11,7 +12,6 @@ app.set('views', `${__dirname}/templates`);
 expressNunjucks(app, {
         watch: true,
         noCache: true,
-    
 });
 
 const port = 3000
@@ -20,17 +20,17 @@ class TokenStorage{
 
     static add_token(token, phone_number){
         this.tokens[token] = {
-            'phone_number':phone_number,
-            'last_updated':Date.now(),
-            'token':token.toUpperCase()
+            'phone_number': phone_number,
+            'last_updated': Date.now(),
+            'token': token.toUpperCase()
         }
     }
 
     static token_is_valid(token){
-        return token.toUpperCase in TokenStorage.tokens 
+        return token in TokenStorage.tokens
     }
 
-    static clear_token(_token){
+    static clear_token(token){
         delete TokenStorage.tokens.token
     }
 }
@@ -41,18 +41,18 @@ app.get('/', (_req, res) => {
     res.render('index');
 });
 
-app.post('/request', (_req, res) => {
-    let phone_number = request.body.phone
+app.post('/request', (req, res) => {
+    let phone_number = req.body.phone
                         .replace('-','').replace('.','')
                         .replace('(','').replace(')','')
                         .replace(' ','')
     let generated_token = get_random_token_hex(config.TOKEN_LENGTH)
     TokenStorage.add_token(generated_token, phone_number)
 
-    telnyx.Message.create({
-     "to": `${config.COUNTRY_CODE}+phone_number`,
-     "from": `${config.FROM_NUMBER}`,
-     "text": "Your Token is "+ generated_token
+    telnyx.messages.create({
+     "to": `${phone_number}`,
+     "from": `${config.COUNTRY_CODE}${config.FROM_NUMBER}`,
+     "text": `Your Token is ${generated_token}`
     })
 
     res.render('verify.html');
@@ -69,9 +69,8 @@ app.post("/verify", (req, res) => {
 })
 
 function get_random_token_hex(num_chars) {
-    require('crypto').randomBytes(num_chars, (_err,buff) => {
-        return buff.toString('hex')
-    })
+    let bytes = crypto.randomBytes(num_chars).toString('hex');
+    return bytes
 }
 
 
